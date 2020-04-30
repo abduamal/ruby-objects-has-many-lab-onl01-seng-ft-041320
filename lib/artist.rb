e1673529825217a5d7076bf5ce5e08ad5dd5aae5
Amal Abdu <abduamal@ymail.com>
2020-04-30 11:13:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,14 +8,18 @@ class Artist
     @songs = []
   end
 
+<<<<<<< HEAD
   def self.song_count
     @@song_count
   end
 
+=======
+>>>>>>> 3ed5070a1d2b94c2495f9a6c7a3581b2ed1aa91c
   def songs
     Song.all.select { |song| song.artist == self}
   end
 
+<<<<<<< HEAD
   def add_song(song_name)
     song_name.artist = self
     @@song_count += 1
@@ -29,4 +33,22 @@ class Artist
     @@song_count += 1
   end
 
+=======
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+>>>>>>> 3ed5070a1d2b94c2495f9a6c7a3581b2ed1aa91c
 end
--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     save
   end
 
@@ -24,4 +25,21 @@ class Song
     end
   end
 
+=======
+    @@all << self
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+>>>>>>> 3ed5070a1d2b94c2495f9a6c7a3581b2ed1aa91c
 end
